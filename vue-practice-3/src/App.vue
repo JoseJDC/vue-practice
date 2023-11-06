9d1fcaf3a7422ed7a3a890d17054a862ec1c8029
JoseJDC <josederas021@gmail.com>
2023-11-06 00:08:53 -0600

    TicTacToe Vue TypeScript

---
+++ vue-practice-3/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TicTacToe from './components/TicTacToe.vue'
+</script>
+
+<template>
+  <h1 class="tittle">TicTacToe</h1>
+  <TicTacToe msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.tittle{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ vue-practice-3/src/components/TicTacToe.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+
+let cells:Ref<Array<string>> = ref(Array(9).fill(null));
+</script>
+<template>
+  <main class="game">
+    <div class="board">
+      <div class="cell" v-for="cell in cells">{{ cell }}</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.game{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.board{
+  display: grid;
+  width: 300px;
+  height: 300px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  gap: 10px;
+}
+.cell {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  border: 4px solid white;
+  border-radius: 15px;
+}
+.cell:hover{
+  border-color: red;
+}
+</style>
